0572caf3b87b93dc815c6ac5fd968b3dc9463161
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-09-28 18:22:45 +0530

    Userinfo view

---
+++ GHFollowers/CustomViews/Views/GFEmptyStateView.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Mukthar Amiyan on 22/09/24.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Mukthar Amiyan on 28/09/24.
+//
+
+import Foundation
