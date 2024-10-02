198ef2a6db3706c4c710a0042c907235329ce5be
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-10-02 20:29:46 +0530

    Refactor on custom views.

---
+++ GHFollowers/CustomViews/Views/GFAlertContainerView.swift
@@ -0,0 +1,34 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Mukthar Amiyan on 02/10/24.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure ()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+     
+}
